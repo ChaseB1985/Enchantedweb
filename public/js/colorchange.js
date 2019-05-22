4d500d9e017ec04ed256e45e5a931e87c53c0f86
matthewbarrier <barriermatt@gmail.com>
2019-05-22 04:02:43 -0500

    styled navbar added updated nav to each page added images

---
+++ public/js/colorchange.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.change').on({
+        mouseenter: function() {
+            $(this).css('background-color', 'yellow');
+            $(this).css('font-size', '24px');
+            $(this).css('color', 'black');
+            $(this).css('font-family', 'Vidaloka', 'serif');
+        },
+        mouseleave: function() {
+            $(this).css('background-color', 'black');
+            $(this).css('font-size', '24px');
+            $(this).css('color', 'yellow');
+            $(this).css('font-family', 'Vidaloka', 'serif');
+        }
+    });
+});  
