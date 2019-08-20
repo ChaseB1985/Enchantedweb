5528cd4e7b78172379cedb6fe5c2633e3dfbc382
chaseb1985 <chasebarrier85@gmail.com>
2019-08-20 13:27:36 -0500

    updated layout

--- public/js/colorchange.js
+++ public/js/colorchange.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.change').on({
-        mouseenter: function() {
+        mouseenter: function () {
             $(this).css('background-color', 'yellow');
             $(this).css('font-size', '24px');
-            $(this).css('color', 'darkgrey');
+            $(this).css('color', '#0a0a0a');
             $(this).css('font-family', 'Vidaloka', 'serif');
         },
-        mouseleave: function() {
-            $(this).css('background-color', 'darkgrey');
+        mouseleave: function () {
+            $(this).css('background-color', '#0a0a0a');
             $(this).css('font-size', '24px');
             $(this).css('color', 'yellow');
             $(this).css('font-family', 'Vidaloka', 'serif');
